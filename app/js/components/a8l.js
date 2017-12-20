611dda51d1364c31c4f42ba3827e6eb8adf49d76
cheil.nelson <nelson.chen@samsung.com>
2017-12-20 12:43:04 +0800

    playsinline

--- app/js/components/a8l.js
+++ app/js/components/a8l.js
@@ -19,7 +19,7 @@ app.partial.a8l = function($, container){
 			ga('send', 'pageview', { 'page': 'PC_1.1.1.1_A8_Technology', 'title': 'PC_1.1.1.1_A8_Technology'});			
 		}
 		//
-        var iframe = '<div><iframe frameborder="0" playsinline="1" allowfullscreen="1" gesture="media" allow="encrypted-media" title="YouTube video player" width="640" height="360" src="https://www.youtube.com/embed/{{vid}}?autoplay=1&amp;playlist={{vid}}&amp;rel=0&amp;controls=0&amp;showinfo=0&amp;modestbranding=1&amp;iv_load_policy=3&amp;enablejsapi=1&amp;version=3&amp;widgetid=1"></iframe></div>';
+        var iframe = '<div><iframe frameborder="0" allowfullscreen="1" gesture="media" allow="encrypted-media" title="YouTube video player" width="640" height="360" src="https://www.youtube.com/embed/{{vid}}?autoplay=1&amp;playlist={{vid}}&amp;rel=0&amp;controls=0&amp;showinfo=0&amp;modestbranding=1&amp;iv_load_policy=3&amp;enablejsapi=1&amp;version=3&amp;widgetid=1&amp;playsinline=1"></iframe></div>';
         $('a[data-vid]').each(function(){
             var vid = $(this).data('vid');
             $(this).html(iframe.replace(/{{vid}}/ig, vid));
